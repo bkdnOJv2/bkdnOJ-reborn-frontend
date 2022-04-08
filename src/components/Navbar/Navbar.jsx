c1f61e9b5a05b4480ba4dd0bfa94856c5a206ab1
nvatuan <24392632+nvatuan@users.noreply.github.com>
2022-04-08 19:26:31 +0700

    Switch to sass, init files

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Navbar.scss'
+
+export default class Navbar extends React.Component {
+
+	render() {
+		return (
+			<div>
+				<ul>
+					<li>Test 1</li>
+					<li>Test 2</li>
+					<li>Test 3</li>
+					<li>Test 4</li>
+				</ul>
+			</div>
+		)
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
+import {Navbar} from './components/index.js'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Navbar/>
+    <p>Hello, world!</p>
   </React.StrictMode>,
   document.getElementById('root')
 );
