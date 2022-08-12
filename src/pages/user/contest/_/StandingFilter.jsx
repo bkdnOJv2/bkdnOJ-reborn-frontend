7d61964f02fdbfca7eb50d178993bbbd2f1738bc
dvt <45824015+trinhdvt@users.noreply.github.com>
2022-08-12 10:33:36 +0000

    Add favorite team filter

--- src/pages/user/contest/_/StandingFilter.jsx
+++ src/pages/user/contest/_/StandingFilter.jsx
@@ -1,74 +1,112 @@
-import React from 'react';
+import React from "react";
 import {Button, Dropdown} from "react-bootstrap";
 
-import './ContestStanding.scss';
-import 'styles/Ratings.scss';
+import "./ContestStanding.scss";
+import "styles/Ratings.scss";
 import "./StandingFilter.scss";
 import {FaFilter} from "react-icons/fa";
+import {BiTrash} from "react-icons/bi";
 
-const StandingFilter = ({orgList, onSave, onToggle, isFilterEnable}) => {
+const ClearIcon = props => {
+  return <BiTrash className="clear-icon" size={18} {...props} />;
+};
+
+const StandingFilter = ({
+  orgList,
+  onSave,
+  onToggleOrgFilter,
+  onToggleFavoriteFilter,
+  isOrgFilterEnable,
+  isFavoriteEnable,
+}) => {
   const [selectedOrg, setSelectedOrg] = React.useState([]);
 
-  const onSelectChange = (e) => {
+  const onOrgFilterSelectChange = e => {
     let selectedIds = [];
     const options = e.target.options;
     for (let i = 0; i < options.length; i++) {
-      if (options[i].selected) {
-        selectedIds.push(options[i].value);
-      }
+      if (options[i].selected) selectedIds.push(options[i].value);
     }
     setSelectedOrg(selectedIds);
-  }
+  };
 
-  const onFilterToggle = (e) => onToggle(e.target.checked)
+  const toggleOrgFilter = e => onToggleOrgFilter(e.target.checked);
+  const toggleFavoriteFilter = e => onToggleFavoriteFilter(e.target.checked);
+  const onSaveClick = () => onSave(selectedOrg);
 
-  const onSaveClick = () => onSave(selectedOrg)
-
-  const onClearClick = () => {
+  const onClearOrgFilter = () => {
     onSave([]);
-    onToggle(false);
+    onToggleOrgFilter(false);
     setSelectedOrg([]);
-  }
+  };
+
+  const onClearFavoriteFilter = () => onToggleFavoriteFilter(false, true);
 
   return (
     <Dropdown>
-      <Dropdown.Toggle variant={isFilterEnable ? "primary" : "secondary"} id="dropdown-basic" className="btn-svg">
-        <FaFilter size={18}/> Filter
+      <Dropdown.Toggle
+        variant={
+          isOrgFilterEnable || isFavoriteEnable ? "primary" : "secondary"
+        }
+        className="btn-svg"
+      >
+        <FaFilter size={18} /> Filter
       </Dropdown.Toggle>
       <Dropdown.Menu>
         <div id="standing-filter">
           <div className="filter-col org-filter">
             <div className="filter-label">
+              <span className="mr-1">Filter by </span>
               <label>
                 <input
                   type="checkbox"
-                  id="org-filter-checkbox"
-                  onChange={onFilterToggle}
-                  checked={isFilterEnable}
+                  onChange={toggleOrgFilter}
+                  checked={!!isOrgFilterEnable}
                 />
                 <span>Organization</span>
               </label>
+              <ClearIcon onClick={onClearOrgFilter} />
             </div>
-            <select multiple name="orgs-filter" className="filter-container border rounded" onChange={onSelectChange}>
+            <select
+              multiple
+              name="org-filter"
+              className="filter-container border rounded"
+              onChange={onOrgFilterSelectChange}
+            >
               {orgList?.map(org => {
-                  return <option key={`opt-org-${org.slug}`} value={org.slug}
-                                 selected={selectedOrg.includes(org.slug)}
-                                 className="filter-option">
+                return (
+                  <option
+                    key={`opt-org-${org.slug}`}
+                    value={org.slug}
+                    selected={selectedOrg.includes(org.slug)}
+                    className="filter-option"
+                  >
                     {`(${org.slug}) ${org.name}`}
-                  </option>;
-                }
-              )}
+                  </option>
+                );
+              })}
             </select>
           </div>
-
+          <div className="filter-label">
+            <label>
+              <input
+                type="checkbox"
+                checked={isFavoriteEnable}
+                onChange={toggleFavoriteFilter}
+              />
+              <span>Favorite Only</span>
+            </label>
+            <ClearIcon onClick={onClearFavoriteFilter} />
+          </div>
           <div className="filter-control-btn">
-            <Button size="sm" onClick={onSaveClick}>Save</Button>
-            <Button variant="warning" size="sm" onClick={onClearClick}>Clear</Button>
+            <Button size="sm" onClick={onSaveClick}>
+              Save
+            </Button>
           </div>
         </div>
       </Dropdown.Menu>
     </Dropdown>
-  )
+  );
 };
 
 export default StandingFilter;
