385b452fec02516e32d7c634771f11b1e8b3441e
dvt <trinhvideo123@gmail.com>
2022-08-15 16:05:59 +0700

    Save standing filter in Redux + Fix UI bug

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,23 +1,25 @@
-import { combineReducers } from 'redux';
-import counterReducer from './Counter/reducer.js';
+import {combineReducers} from "redux";
+import counterReducer from "./Counter/reducer.js";
 
-import userReducer from './User/reducer.js';
-import profileReducer from './Profile/reducer.js';
-import contestReducer from './Contest/reducer';
+import userReducer from "./User/reducer.js";
+import profileReducer from "./Profile/reducer.js";
+import contestReducer from "./Contest/reducer";
+import standingFilterReducer from "./StandingFilter/reducer";
 
-import recentSubmissionReducer from './RecentSubmission/reducer';
-import ranksReducer from './Rank/reducer';
-import myOrgReducer from './MyOrg/reducer';
+import recentSubmissionReducer from "./RecentSubmission/reducer";
+import ranksReducer from "./Rank/reducer";
+import myOrgReducer from "./MyOrg/reducer";
 
 const rootReducer = combineReducers({
-    counter: counterReducer,
-    user: userReducer,
-    profile: profileReducer,
-    contest: contestReducer,
-    recentSubmission: recentSubmissionReducer,
-    ranks: ranksReducer,
+  counter: counterReducer,
+  user: userReducer,
+  profile: profileReducer,
+  contest: contestReducer,
+  recentSubmission: recentSubmissionReducer,
+  ranks: ranksReducer,
 
-    myOrg: myOrgReducer,
+  myOrg: myOrgReducer,
+  standingFilter: standingFilterReducer,
 });
 
 export default rootReducer;
