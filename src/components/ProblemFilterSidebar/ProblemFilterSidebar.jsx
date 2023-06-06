a50f2d67e9881d6af25d998c1b3d7ab967533c66
nvatuan <24392632+nvatuan@users.noreply.github.com>
2023-06-07 02:32:17 +0700

    Fix css

--- src/components/ProblemFilterSidebar/ProblemFilterSidebar.jsx
+++ src/components/ProblemFilterSidebar/ProblemFilterSidebar.jsx
@@ -35,44 +35,32 @@ const DATA = [
 
 const ProblemFilterSidebar = (props) => {
   return (
-      <div className="wrapper-vanilla" id="sub-filter">
-        <h4>Problem Filter</h4>
-        {(
-          <>
-            <div className="flex-center-col text-left filter-panel">
-              <Row> Tags: </Row>
-              <Row>
-                  <Select isMulti closeMenuOnSelect={false} options={DATA}/>
-              </Row>
-              <div className="w-100 checkbox-panel" style={{columnGap: "5px"}}>
-                {(
-                  <label
-                    id="only-me-lbl"
-                    className="d-flex align-items-center m-0"
-                  >
-                    <input
-                      type="checkbox"
-                      id="only-me"
-                      className="ml-1 mr-1"
-                      checked={false}
-                    ></input>
-                    <span style={{flex: 2}}>Mine</span>
-                  </label>
-                )}
+    <div className="wrapper-vanilla" id="sub-filter">
+      <h4>Problem Filter</h4>
+      {(
+        <>
+          <div className="flex-center-col text-left filter-panel p-2">
+            <Row>
+              Problem Tags:
+            </Row>
+            <Row>
+              <div className="unset-css-select-menu">
+                <Select isMulti closeMenuOnSelect={false} options={DATA} placeholder="Select.." />
               </div>
-            </div>
+            </Row>
+          </div>
 
-            <div className="p-1 text-right d-flex flex-row-reverse" style={{ columnGap: "5px", }}>
-              <Button size="sm" variant="light" className="btn-svg">
-                <FaTimes /> Clear
-              </Button>
-              <Button size="sm" variant="secondary" className="btn-svg">
-                <FaFilter /> Filter
-              </Button>
-            </div>
-          </>
-        )}
-      </div>
+          <div className="p-1 text-right d-flex flex-row-reverse" style={{ columnGap: "5px", }}>
+            <Button size="sm" variant="light" className="btn-svg">
+              <FaTimes /> Clear
+            </Button>
+            <Button size="sm" variant="secondary" className="btn-svg">
+              <FaFilter /> Filter
+            </Button>
+          </div>
+        </>
+      )}
+    </div>
   )
 }
 
